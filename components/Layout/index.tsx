b2b6c8183e3479cd09e7ed7e773a5014a21c2ece
ptbird <ptbird@yeah.net>
2022-07-20 01:26:27 +0800

    feat: router

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 import styles from './index.module.css';
 
@@ -24,12 +25,19 @@ const routes = [
 export const Layout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+  const router = useRouter();
+  console.log('router', router);
   return (
     <div className={styles.wrap}>
       <h1 className={styles.title}>Hello Next.js</h1>
       <div className={styles.linkWrap}>
         {routes.map((item) => (
-          <div className={styles.link} key={item.path}>
+          <div
+            className={`${styles.link} ${
+              item.path === router.asPath ? styles.active : ''
+            }`}
+            key={item.path}
+          >
             <Link href={item.path}>{item.label}</Link>
           </div>
         ))}
