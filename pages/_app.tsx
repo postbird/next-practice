32b40bc7e53c38fad982fc5a0dfb14738789efce
ptbird <ptbird@yeah.net>
2022-08-08 23:32:39 +0800

    feat: init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,9 @@
 import 'antd/dist/antd.min.css';
-import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
-import { ReactElement, ReactNode } from 'react';
-import { AppLayout } from '../components';
 import '../styles/globals.css';
 
-// 扩展 NextPage 属性
-export type NextLayoutPage<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-// 扩展 AppProps 中的 Component 属性为 NextLayoutPage
-export type AppLayoutProps = AppProps & {
-  Component: NextLayoutPage;
-};
-
-function MyApp({ Component, pageProps }: AppLayoutProps) {
-  const getLayout = Component.getLayout ?? ((page) => page);
-
-  return <AppLayout>{getLayout(<Component {...pageProps} />)}</AppLayout>;
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
-
-// export function reportWebVitals(metric: any) {
-//   console.log(metric);
-// }
 
 export default MyApp;
