0d3a252381c539570bbd5fc5585700a057692c44
ptbird <ptbird@yeah.net>
2022-08-10 00:48:15 +0800

    feat: lint test

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,10 +1,33 @@
 import { GetStaticProps, NextPage } from 'next';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 import styles from './index.module.less';
 
 const Page: NextPage<{ posts: number[] }> = ({ posts }) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div className={styles.wrap}>
-      <h1>Posts</h1>
+      <h1>Posts: {count}</h1>
+      <Image
+        src="/images/large.jpg"
+        alt="large"
+        width={400}
+        height={300}
+        objectFit={'cover'}
+      />
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src="/images/large.jpg" alt="xx" width={400} height={300} />
     </div>
   );
 };
