b571f0c0285d57cc4e5d3bcb8f24463ce76693c5
ptbird <ptbird@yeah.net>
2022-08-10 00:12:25 +0800

    feat: font

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,31 +1,10 @@
 import { GetStaticProps, NextPage } from 'next';
-import Image from 'next/image';
 import styles from './index.module.less';
-
-const requireImg = require('../../public/images/large.jpg');
-
-const remoteImg =
-  'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg';
-const localImg = '/images/large.jpg';
 
 const Page: NextPage<{ posts: number[] }> = ({ posts }) => {
   return (
     <div className={styles.wrap}>
       <h1>Posts</h1>
-      {/* {posts?.map((item) => (
-        <div key={item}>
-          <Image
-            src={localImg}
-            width={400}
-            height={400}
-            // layout="fill"
-            alt={`post-${item}`}
-          />
-        </div>
-      ))} */}
-      {posts?.map((item) => (
-        <Image src={requireImg} key={item} alt={`post-${item}`} priority />
-      ))}
     </div>
   );
 };
