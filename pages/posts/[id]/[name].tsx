6d44fb96994ab2008cc66e954993942083d3f943
ptbird <ptbird@yeah.net>
2022-08-11 01:24:37 +0800

    feat: route

---
+++ pages/posts/[id]/[name].tsx
@@ -0,0 +1,18 @@
+import { PostItem } from '@/components';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import styles from '../index.module.less';
+
+const Page: NextPage = (props) => {
+  const router = useRouter();
+  console.log('router', router.query);
+
+  return (
+    <div className={styles.wrap}>
+      <h1>Post Slug Page [id]/[name]tsx</h1>
+      <PostItem />
+    </div>
+  );
+};
+
+export default Page;
